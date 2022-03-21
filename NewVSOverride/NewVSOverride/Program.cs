bfc0ad79a778615b2c1bea0c8206522eae511553
Anatoly <petrovanatoly2000@gmail.com>
2022-03-21 16:51:29 +0300

    Сравнение оператора new и override
    Написал программу для закрепления материала по данной теме

---
+++ NewVSOverride/NewVSOverride/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace NewVSOverride
+{
+    //Если вы работаете с экземпляром класса-наследника через его родительский класс,
+    //то в случае, если вы будете вызывать переопределенный виртуальный метод(override),
+    //то будет вызвана его реализация из наследника, а если перекрытый(new), то будет вызван метод базового класса.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man man = new Child("Вася");
+            Man man2 = new Man("Василий");
+            Child child = (Child)man;
+
+            //child = (Child)man2;
+            // сверху down-cast преобразование невозможно
+            child.First(); // выведет метод First у Child
+            child.Second(); // выведет метод Second у Child
+
+            int i = 0;
+            Console.WriteLine(i++);
+            Console.Write(i++ + Calculate(i));
+            Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+
+        public static int Calculate(int i)
+        {
+            Console.Write(i++);
+            return i;
+        }
+    }
+
+
+    class Man
+    {
+        public string Name { get; set; }
+        public Man (string name)
+            {
+            Name = name;
+            }
+        public virtual void First()
+        {
+            Console.WriteLine($"First from Man {Name}");
+        }
+
+        public void Second()
+        {
+            Console.WriteLine($"Second from Man {Name}");
+        }
+    }
+
+    class Child : Man
+    {
+        public Child(string name): base(name)
+        {
+        }
+        public override void First()
+        {
+            Console.WriteLine($"First from Child {Name}");
+        }
+
+        public new void Second()
+        {
+            Console.WriteLine($"Second from Child {Name}");
+        }
+    }
+}
