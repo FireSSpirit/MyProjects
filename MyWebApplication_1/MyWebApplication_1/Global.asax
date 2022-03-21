127206232ce3efdaaa138cb384816dc0a5c63e2e
Anatoly <petrovanatoly2000@gmail.com>
2022-03-21 19:56:16 +0300

    Создал веб приложение для практики
    c использованием ASP NET MVC

---
+++ MyWebApplication_1/MyWebApplication_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyWebApplication_1.MvcApplication" Language="C#" %>
---
+++ MyWebApplication_1/MyWebApplication_1/Models/ForumContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MyWebApplication_1.Models
+{
+    public class ForumContext:DbContext
+    {
+        public DbSet<ForumTheme> ForumThemes { get; set; }
+    }
+}
