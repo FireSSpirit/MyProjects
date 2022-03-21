e734259174205b68fd3c3a98247ae8c3e0ca14e0
Anatoly <petrovanatoly2000@gmail.com>
2022-03-21 16:53:08 +0300

    Изучил применение await и async
    С помощью секундомера проверил и убедился в эффективности асинхронного программирования

---
+++ AsyncEx/AsyncEx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+namespace AsyncEx
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch(); // секундомер
+            stopWatch.Start(); // запустить секундомер
+            Task tomTask = PrintNameAsync("Tom");
+            var bobTask = PrintNameAsync("Bob");
+            var samTask = PrintNameAsync("Sam");
+
+            await tomTask;
+            await bobTask;
+            await samTask;
+            Console.WriteLine();
+            stopWatch.Stop(); // остановить секундомер
+
+            TimeSpan ts = stopWatch.Elapsed; // структура для работы с временем
+            Console.WriteLine($"{ts.Seconds:00}:{ts.Milliseconds:00}"); // вывод секунд и милисекунд
+            async Task PrintNameAsync(string name)
+            {
+                await Task.Delay(3000);     // имитация продолжительной работы
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
