71fe2ed0013646d77894b1abafec89eadf95b09f
Anatoly <petrovanatoly2000@gmail.com>
2022-04-14 22:12:42 +0300

    Тестирование получения адреса
    Написал программу для изучения темы dns серверов и ip адресов.

---
+++ AddressTest/AddressTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace AddressTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry host1 = Dns.GetHostEntry("www.youtube.com");
+            Console.WriteLine(host1.HostName);
+            foreach (IPAddress ip in host1.AddressList)
+                Console.WriteLine(ip.ToString());
+
+            Console.WriteLine();
+
+            IPHostEntry host2 = Dns.GetHostEntry("yandex.ru");
+            Console.WriteLine(host2.HostName);
+            foreach (IPAddress ip in host2.AddressList)
+                Console.WriteLine(ip.ToString());
+        }
+    }
+}
